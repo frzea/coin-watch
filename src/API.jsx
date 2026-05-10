2a104a6f42dbebc8a8b5bdca77c81fef770fffec
Ostapenko Dima <frzeacode@gmail.com>
2026-05-10 18:25:51 +0300

    V 1.0 get api and add select in localStorage

---
+++ src/API.jsx
@@ -0,0 +1,13 @@
+export const API =
+[
+{id: 'bitcoin', symbol: 'btc', name: 'Bitcoin', image: 'https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400', current_price: 80923}, 
+{id: 'ethereum', symbol: 'eth', name: 'Ethereum', image: 'https://coin-images.coingecko.com/coins/images/279/large/ethereum.png?1696501628', current_price: 2330.66},
+{id: 'tether', symbol: 'usdt', name: 'Tether', image: 'https://coin-images.coingecko.com/coins/images/325/large/Tether.png?1696501661', current_price: 0.99982}, 
+{id: 'ripple', symbol: 'xrp', name: 'XRP', image: 'https://coin-images.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1696501442', current_price: 1.43},
+{id: 'binancecoin', symbol: 'bnb', name: 'BNB', image: 'https://coin-images.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970', current_price: 651.22},
+{id: 'usd-coin', symbol: 'usdc', name: 'USDC', image: 'https://coin-images.coingecko.com/coins/images/6319/large/USDC.png?1769615602', current_price: 0.999908},
+{id: 'solana', symbol: 'sol', name: 'Solana', image: 'https://coin-images.coingecko.com/coins/images/4128/large/solana.png?1718769756', current_price: 93.89},
+{id: 'tron', symbol: 'trx', name: 'TRON', image: 'https://coin-images.coingecko.com/coins/images/1094/large/photo_2026-04-13_09-59-16.png?1776048311', current_price: 0.351151},
+{id: 'figure-heloc', symbol: 'figr_heloc', name: 'Figure Heloc', image: 'https://coin-images.coingecko.com/coins/images/68480/large/figure.png?1755863954', current_price: 1},
+{id: 'dogecoin', symbol: 'doge', name: 'Dogecoin', image: 'https://coin-images.coingecko.com/coins/images/5/large/dogecoin.png?1696501409', current_price: 0.108116}
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,77 @@
 import { useState, useEffect } from 'react'
+import { CoinList } from './coin-list.jsx'
+import { API } from './API.jsx'
 import './App.css'
 
 
 export default function App(){
+  const [allCoins, setAllCoins] = useState([]);
+  const [userCoins, setUserCoins] = useState([]);
+
+  //localStorage.clear();
+  console.log(userCoins);
 
   useEffect(() => {
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10')
-        .then(res => res.json())
-        .then(data => console.log(data))
-  }, [])
+    /*async function getData() {
+      const dataJSON = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10')
+        .then(res => res.json())  
+        console.log(dataJSON);
+        setAllCoins(dataJSON);
+    }
+    getData();*/
+    setAllCoins(API);
+  }, []);
+
+
+  function AddToLocalStorage( coin ){
+
+    const current = JSON.parse(localStorage.getItem('coins')) || [];
+    if(current.includes(coin)) return; // уже есть
+
+    const updated = [...current, coin];
+    localStorage.setItem('coins', JSON.stringify(updated));
+    setUserCoins(updated);
+
+    /*if(localStorage.getItem('coins') === null ){
+        localStorage.setItem('coins',JSON.stringify([coin]));
+    } else {
+      localStorage.setItem('coins', JSON.stringify([...JSON.parse(localStorage.getItem('coins')), coin]));
+    };
+    setUserCoins(JSON.parse(localStorage.getItem('coins')));*/
+  }
+
 
   return ( 
-    <button> форму</button>
+    <>
+      <input placeholder="Add item" />
+      <button>Добавить</button>
+      <div>
+        {allCoins.map(coin => (
+            <li key={coin.id}> 
+              <label>
+              <img key={coin.id} src={coin.image} alt={coin.symbol} width="15" height="15" />
+              {coin.name} --  {coin.current_price}
+              </label>
+              <button onClick={()=>{AddToLocalStorage(coin.symbol)}}>+</button>
+              <button>-</button>
+            </li>
+          ) 
+        )}
+      </div>
+      <hr/>
+        <div>
+        {[...allCoins].filter(coin => userCoins.includes(coin.symbol)).map(coin => (
+            <li key={coin.id}> 
+              <label>
+              <img key={coin.id} src={coin.image} alt={coin.symbol} width="15" height="15" />
+              {coin.name} --  {coin.current_price}
+              </label>
+              <button onClick={()=>{AddToLocalStorage(coin.symbol)}}>+</button>
+              <button>-</button>
+            </li>
+          ) 
+        )}
+      </div>
+    </>
   );
 }
