7e8fa1bf4d69ce95e8d329349a07ed7311107c67
Ostapenko Dima <frzeacode@gmail.com>
2026-06-05 23:51:29 +0300

    V 1.3.1 migration to zustand continuation

--- src/components/coin-page/components/coin-tools/pnl/components/add-position-form/index.tsx
+++ src/components/coin-page/components/coin-tools/pnl/components/add-position-form/index.tsx
@@ -1,12 +1,16 @@
-import { AddPositionFormProps } from './type.ts'
+import { AddPositionFormProps } from './type.ts';
+import { useCoinStore } from '../../../../../../../store/CoinStore.ts';
+import { useCoinToolsStore } from '../../../../../../../store/CoinToolsStore.ts'
 
 export function AddPositionForm({newPosition,setNewPosition,handleAddPosition}: AddPositionFormProps){
+   const { selectCoinId } = useCoinStore();
+   const { addPosition } = useCoinToolsStore();
    return(
       <>
          Количество: <input type="number" min={0} value={newPosition.qty} onChange={e => setNewPosition({...newPosition, qty : Number(e.target.value)})}/><br/>
          Цена: <input type="number" min={0} value={newPosition.price} onChange={e => setNewPosition({...newPosition, price : Number(e.target.value)})}/><br/>
          Дата: <input type="date" value={newPosition.date} onChange={e => setNewPosition({...newPosition, date : e.target.value})}/><br/>
-         <button onClick={()=>{handleAddPosition()}}>Добавить</button>
+         <button onClick={() => addPosition(selectCoinId, newPosition) }>Добавить</button>
       </>
    )
 }
